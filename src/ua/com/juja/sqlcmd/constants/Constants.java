a63a3741e6cdd35c5b176e4418d4d4d8cef393e9
MAlex <alexmaximovj@gmail.com>
2015-10-04 23:04:36 +0300

    Add to project layout dao, dependency PostgreSQL JDBC Driver  and interface Constants

---
+++ src/ua/com/juja/sqlcmd/constants/Constants.java
@@ -0,0 +1,13 @@
+package ua.com.juja.sqlcmd.constants;
+
+/**
+ * Created by ION on 04.10.2015.
+ */
+public interface Constants {
+    public static final String CONNECTING_URL = "jdbc:postgresql://localhost:5432/sqlcmd";
+    public static final String USER = "postgres";
+    public static final String PASSWORD = "postgres";
+
+    public static final int CONNECTING_POOL_SIZE = 5;
+
+}
